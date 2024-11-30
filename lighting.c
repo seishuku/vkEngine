545bc6c1b79ffe128db00b5cb950432bf0775707
Matt Williams <nitrogl@gmail.com>
2024-11-29 22:48:05 -0600

    Minor tweak

--- lighting.c
+++ lighting.c
@@ -37,7 +37,7 @@ renderPass {
 				.format=colorFormat,
 				.samples=MSAA,
 				.loadOp=VK_ATTACHMENT_LOAD_OP_CLEAR,
-				.storeOp=VK_ATTACHMENT_STORE_OP_STORE,
+				.storeOp=VK_ATTACHMENT_STORE_OP_DONT_CARE,
 				.stencilLoadOp=VK_ATTACHMENT_LOAD_OP_DONT_CARE,
 				.stencilStoreOp=VK_ATTACHMENT_STORE_OP_DONT_CARE,
 				.initialLayout=VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
