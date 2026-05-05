f364b3ad396a485636b7f8001534cde0d131f0c4
Matt Williams <nitrogl@gmail.com>
2026-05-05 17:45:44 -0500

    Increase impact speed for game logic

--- engine.c
+++ engine.c
@@ -1113,7 +1113,7 @@ void Thread_Physics(void *arg)
 				float impactSpeed=PhysicsResolveCollision(manifold->a, manifold->b, manifold->contacts[j]);
 
 				// Run "game logic"
-				if(impactSpeed>1.5f)
+				if(impactSpeed>1.75f)
 				{
 					DBGPRINTF(DEBUG_INFO, "Impact speed: %f\n", impactSpeed);
 					// If both objects are asteroids
